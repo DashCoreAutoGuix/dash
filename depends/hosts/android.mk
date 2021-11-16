1e2a1af3f42802aa33128edb3f4bef2ce73af658
fanquake <fanquake@gmail.com>
2021-11-16 10:05:02 +0800

    Merge bitcoin/bitcoin#23478: build: Add support for Android NDK r23 LTS
    4ba492052ec09d48f8c3f391cc248340e761c7f2 doc: Add minimum supported Android NDK version (Hennadii Stepanov)
    6393bdcd53b106367b10317c227a114494c90142 doc: Move Android dependencies guide into `build-android.md` (Hennadii Stepanov)
    ac323a7222efaafc7bc3110b02f1ef2d2635c9a2 build: Switch to llvm buinutils for Android builds (Hennadii Stepanov)
    
    Pull request description:
    
      The new Long Term Support release of the Android NDK is [available](https://groups.google.com/g/android-ndk-announce/c/MS6Qoub0DKE/m/Zfp5Ys8eAAAJ) since 2021-08-11:
    
      > As r23 is the new LTS, the support windows for r21 and r22 have now ended.
    
      On master (8ae4ba481ce8f7da173bef24432729c87a36cb70), dependency build fails because it expects GNU Binutils are present in the Android NDK. In [fact](https://android.googlesource.com/platform/ndk/+/master/docs/BuildSystemMaintainers.md#binutils):
      > GNU Binutils remains available up to and including r22. All binutils tools with the exception of the assembler (GAS) were removed in r23. GAS was removed in r24.
    
      This PR switches our depends build system to llvm binutils. The usage of `llvm-ar` and `llvm-ranlib` tools effectively makes r21 the minimum supported version of NDK.
    
      With this PR:
      - building depends against NDK r23 LTS now is possible with `NO_QT=1`
      - building the `qt` package in depends against NDK r23 LTS still fails:
      ```
      Creating qmake...
      ...
    
      ERROR: Cannot detect Android NDK toolchain.
      Please use -android-toolchain-version to specify it.
      ```
    
      The issue with the `qt` package is going to be addressed in another PR.
    
    ACKs for top commit:
      fanquake:
        ACK 4ba492052ec09d48f8c3f391cc248340e761c7f2
    
    Tree-SHA512: cdc8f95ff9a3ad7f12eb55b9ea18b6b6b800d4cceff7e0321985be6e39d15a2b2ea5b1592972307d76d111292a0ed58fd287e5ca285e2f6868b42a286536d310

--- depends/hosts/android.mk
+++ depends/hosts/android.mk
@@ -5,7 +5,6 @@ else
 android_CXX=$(ANDROID_TOOLCHAIN_BIN)/$(HOST)$(ANDROID_API_LEVEL)-clang++
 android_CC=$(ANDROID_TOOLCHAIN_BIN)/$(HOST)$(ANDROID_API_LEVEL)-clang
 endif
-
 android_CFLAGS=-std=$(C_STANDARD)
 android_CXXFLAGS=-std=$(CXX_STANDARD)
 
