d3c5204934a3f7d0a7d59c8c17a053b93699619d
MacroFake <falke.marco@gmail.com>
2022-10-27 16:15:12 +0200

    Merge bitcoin/bitcoin#26388: ci: Use `macos-ventura-xcode:14.1` image for "macOS native" task

--- ci/test/00_setup_env_mac_native_arm64.sh
+++ ci/test/00_setup_env_mac_native_arm64.sh
@@ -6,14 +6,11 @@
 
 export LC_ALL=C.UTF-8
 
-export CONTAINER_NAME=ci_macos
-export HOST=x86_64-apple-darwin
-export PIP_PACKAGES="zmq lief"
+export HOST=arm64-apple-darwin
+export PIP_PACKAGES="zmq"
 export GOAL="install"
-export BITCOIN_CONFIG="--with-gui --enable-reduce-exports --disable-miner"
+export BITCOIN_CONFIG="--with-gui --with-miniupnpc --with-natpmp --enable-reduce-exports"
 export CI_OS_NAME="macos"
 export NO_DEPENDS=1
 export OSX_SDK=""
-export CCACHE_MAXSIZE=300M
-
-export RUN_SECURITY_TESTS="true"
+export CCACHE_SIZE=300M
