a9517158b60a37e4d5fc1440be753b5b4663f020
furszy <matiasfurszyfer@protonmail.com>
2022-12-17 09:39:24 -0300

    test,gui: decouple widgets and model into a MiniGui struct
    So it can be reused across tests.

--- src/qt/test/wallettests.cpp
+++ src/qt/test/wallettests.cpp
@@ -205,6 +205,32 @@ std::shared_ptr<CWallet> SetupDescriptorsWallet(interfaces::Node& node, TestChai
     return wallet;
 }
 
+struct MiniGUI {
+public:
+    SendCoinsDialog sendCoinsDialog;
+    TransactionView transactionView;
+    OptionsModel optionsModel;
+    std::unique_ptr<ClientModel> clientModel;
+    std::unique_ptr<WalletModel> walletModel;
+
+    MiniGUI(interfaces::Node& node, const PlatformStyle* platformStyle) : sendCoinsDialog(platformStyle), transactionView(platformStyle), optionsModel(node) {
+        bilingual_str error;
+        QVERIFY(optionsModel.Init(error));
+        clientModel = std::make_unique<ClientModel>(node, &optionsModel);
+    }
+
+    void initModelForWallet(interfaces::Node& node, const std::shared_ptr<CWallet>& wallet, const PlatformStyle* platformStyle)
+    {
+        WalletContext& context = *node.walletLoader().context();
+        AddWallet(context, wallet);
+        walletModel = std::make_unique<WalletModel>(interfaces::MakeWallet(context, wallet), *clientModel, platformStyle);
+        RemoveWallet(context, wallet, /* load_on_start= */ std::nullopt);
+        sendCoinsDialog.setModel(walletModel.get());
+        transactionView.setModel(walletModel.get());
+    }
+
+};
+
 //! Simple qt wallet tests.
 //
 // Test widgets can be debugged interactively calling show() on them and
@@ -220,15 +246,13 @@ std::shared_ptr<CWallet> SetupDescriptorsWallet(interfaces::Node& node, TestChai
 //     QT_QPA_PLATFORM=cocoa   src/qt/test/test_dash-qt  # macOS
 void TestGUI(interfaces::Node& node, const std::shared_ptr<CWallet>& wallet)
 {
-    WalletContext& context = *node.walletLoader().context();
     // Create widgets for sending coins and listing transactions.
-    SendCoinsDialog sendCoinsDialog;
-    TransactionView transactionView;
-    OptionsModel optionsModel;
-    ClientModel clientModel(node, &optionsModel);
-    WalletModel walletModel(interfaces::MakeWallet(context, wallet), clientModel);
-    sendCoinsDialog.setModel(&walletModel);
-    transactionView.setModel(&walletModel);
+    std::unique_ptr<const PlatformStyle> platformStyle(PlatformStyle::instantiate("other"));
+    MiniGUI mini_gui(node, platformStyle.get());
+    mini_gui.initModelForWallet(node, wallet, platformStyle.get());
+    WalletModel& walletModel = *mini_gui.walletModel;
+    SendCoinsDialog& sendCoinsDialog = mini_gui.sendCoinsDialog;
+    TransactionView& transactionView = mini_gui.transactionView;
 
     {
         // Check balance in send dialog
