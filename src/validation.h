beedc6097d9a8b86730efa46c180018d009388bb
fanquake <fanquake@gmail.com>
2023-03-08 08:40:21 +0100

    Merge bitcoin/bitcoin#27189: util: Use steady clock in SeedStrengthen, FindBestImplementation, FlushStateToDisk
    fa1b4e5c3294fc9aec033892a4a4d7b5cfc015aa Use steady clock in FlushStateToDisk (MarcoFalke)
    1111e2f8b43cd9ed62dcf6b571a224b84fc421fd Use steady clock in SeedStrengthen and FindBestImplementation (MarcoFalke)
    
    Pull request description:
    
      There may be a theoretical deadlock for the duration of the offset when the system clock is adjusted into a past time while executing `SeedStrengthen`.
    
      Fix this by using steady clock.
    
      Do the same in `FindBestImplementation`, which shouldn't be affected, because it discards outlier measurements. However, doing the same there for consistency seems fine.
    
      Do the same in `FlushStateToDisk`, which should make the flushes more steady, if the system clock is adjusted by a large offset.
    
    ACKs for top commit:
      john-moffett:
        ACK fa1b4e5c3294fc9aec033892a4a4d7b5cfc015aa
      willcl-ark:
        ACK fa1b4e5c3
    
    Tree-SHA512: cc625e796b186accd53222bd64eb57d0512bc7e588312d254349b542bbc5e5daac348ff2b3b3f7dc5ae0bbbae2ec11fdbf3022cf2164211633765a4b0108e83e

--- src/validation.h
+++ src/validation.h
@@ -797,6 +797,9 @@ private:
     void UpdateTip(const CBlockIndex* pindexNew)
         EXCLUSIVE_LOCKS_REQUIRED(::cs_main);
 
+    SteadyClock::time_point m_last_write{};
+    SteadyClock::time_point m_last_flush{};
+
     friend ChainstateManager;
 };
 
