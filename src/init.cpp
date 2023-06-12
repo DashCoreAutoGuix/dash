07c6ee60851cf6833df39b3a8cba855c01ae4997
Ryan Ofsky <ryan@ofsky.org>
2023-06-12 12:13:55 -0400

    Merge bitcoin/bitcoin#27708: Return EXIT_FAILURE on post-init fatal errors
    61c569ab6069d04079a0831468eb713983919636 refactor: decouple early return commands from AppInit (furszy)
    4927167f855f8ed3bbf6d2766f61229f742e632a gui: return EXIT_FAILURE on post-init fatal errors (furszy)
    3b2c61e8198bcefb1c2343caff1d705951026cc4 Return EXIT_FAILURE on post-init fatal errors (furszy)
    3c06926cf21dcca3074ef51506f556b2286c299b refactor: index: use `AbortNode` in fatal error helper (Sebastian Falbesoner)
    9ddf7e03a35592617a016418fd320cc93c8d1abd move ThreadImport ABC error to use AbortNode (furszy)
    
    Pull request description:
    
      It seems odd to return `EXIT_SUCCESS` when the node aborted execution due a fatal internal error
      or any post-init problem that triggers an unrequested shutdown.
    
      e.g. blocks or coins db I/O errors, disconnect block failure, failure during thread import (external
      blocks loading process error), among others.
    
    ACKs for top commit:
      TheCharlatan:
        ACK 61c569ab6069d04079a0831468eb713983919636
      ryanofsky:
        Code review ACK 61c569ab6069d04079a0831468eb713983919636
      pinheadmz:
        ACK 61c569ab6069d04079a0831468eb713983919636
      theStack:
        Code-review ACK 61c569ab6069d04079a0831468eb713983919636
    
    Tree-SHA512: 18a59c3acc1c6d12cbc74a20a401e89659740c6477fccb59070c9f97922dfe588468e9e5eef56c5f395762187c34179a5e3954aa5b844787fa13da2e666c63d3

--- src/init.cpp
+++ src/init.cpp
@@ -1060,7 +1060,7 @@ std::set<BlockFilterType> g_enabled_filter_types;
     std::terminate();
 };
 
-bool AppInitBasicSetup(const ArgsManager& args)
+bool AppInitBasicSetup(const ArgsManager& args, std::atomic<int>& exit_status)
 {
     // ********************************************************* Step 1: setup
 #ifdef _MSC_VER
@@ -1074,7 +1074,7 @@ bool AppInitBasicSetup(const ArgsManager& args)
     // Enable heap terminate-on-corruption
     HeapSetInformation(nullptr, HeapEnableTerminationOnCorruption, nullptr, 0);
 #endif
-    if (!InitShutdownState()) {
+    if (!InitShutdownState(exit_status)) {
         return InitError(Untranslated("Initializing wait-for-shutdown state failed."));
     }
 
--- src/interfaces/node.h
+++ src/interfaces/node.h
@@ -155,6 +155,9 @@ public:
     //! Get warnings.
     virtual bilingual_str getWarnings() = 0;
 
+    //! Get exit status.
+    virtual int getExitStatus() = 0;
+
     // Get log flags.
     virtual uint64_t getLogCategories() = 0;
 
