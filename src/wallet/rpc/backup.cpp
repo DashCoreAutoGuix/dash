f0a3ca33fe7461d5e7c0a35cee515f4272e29938
Andrew Chow <github@achow101.com>
2023-06-16 15:05:44 -0400

    Merge bitcoin/bitcoin#27757: rpc: remove deprecated "warning" field from {create,load,restore,unload}wallet
    5524fa00faebfe040f126a4152640f9e9ed572b1 doc: add release note about removal of `deprecatedrpc=walletwarningfield` flag (Sebastian Falbesoner)
    5c77db73542fe4c76fd53526ae560d56dde5f830 Restorewallet/createwallet help documentation fixups/improvements (Jon Atack)
    a00ae31fccba63d5fd409ffb39c1622df2ea3e8c rpc: remove deprecated "warning" field from {create,load,restore,unload}wallet (Sebastian Falbesoner)
    
    Pull request description:
    
      The "warning" string field for wallet creating/loading RPCs (`createwallet`, `loadwallet`, `unloadwallet` and `restorewallet`) has been deprecated with the configuration option `-deprecatedrpc=walletwarningfield` in PR #27279 (released in v25.0). For the next release v26.0, the field and the configuration option can be removed.
    
    ACKs for top commit:
      achow101:
        ACK 5524fa00faebfe040f126a4152640f9e9ed572b1
      jonatack:
        ACK 5524fa00faebfe040f126a4152640f9e9ed572b1
    
    Tree-SHA512: 8212f72067d08095304018b8a95d2ebef630004b65123483fbbfb078cc5709c2d825bbc35b16ea5f6b28ae7377347382d7e9afaf7bdbf0575d2c229d970784de

--- src/wallet/rpc/backup.cpp
+++ src/wallet/rpc/backup.cpp
@@ -2057,7 +2057,7 @@ RPCHelpMan listdescriptors()
 RPCHelpMan backupwallet()
 {
     return RPCHelpMan{"backupwallet",
-        "\nSafely copies current wallet file to destination, which can be a directory or a path with filename.\n",
+        "\nSafely copies the current wallet file to the specified destination, which can either be a directory or a path with a filename.\n",
         {
             {"destination", RPCArg::Type::STR, RPCArg::Optional::NO, "The destination directory or file"},
         },
@@ -2091,7 +2091,9 @@ RPCHelpMan restorewallet()
 {
     return RPCHelpMan{
         "restorewallet",
-        "\nRestore and loads a wallet from backup.\n",
+        "\nRestores and loads a wallet from backup.\n"
+        "\nThe rescan is significantly faster if a descriptor wallet is restored"
+        "\nand block filters are available (using startup option \"-blockfilterindex=1\").\n",
         {
             {"wallet_name", RPCArg::Type::STR, RPCArg::Optional::NO, "The name that will be applied to the restored wallet"},
             {"backup_file", RPCArg::Type::STR, RPCArg::Optional::NO, "The backup file that will be used to restore the wallet."},
@@ -2101,7 +2103,10 @@ RPCHelpMan restorewallet()
             RPCResult::Type::OBJ, "", "",
             {
                 {RPCResult::Type::STR, "name", "The wallet name if restored successfully."},
-                {RPCResult::Type::STR, "warning", "Warning message if wallet was not loaded cleanly."},
+                {RPCResult::Type::ARR, "warnings", /*optional=*/true, "Warning messages, if any, related to restoring and loading the wallet.",
+                {
+                    {RPCResult::Type::STR, "", ""},
+                }},
             }
         },
         RPCExamples{
@@ -2131,7 +2136,13 @@ RPCHelpMan restorewallet()
 
     UniValue obj(UniValue::VOBJ);
     obj.pushKV("name", wallet->GetName());
-    obj.pushKV("warning", Join(warnings, Untranslated("\n")).original);
+    UniValue warnings_array(UniValue::VARR);
+    for (const auto& warning : warnings) {
+        warnings_array.push_back(warning.original);
+    }
+    if (!warnings_array.empty()) {
+        obj.pushKV("warnings", warnings_array);
+    }
 
     return obj;
 
--- src/wallet/rpc/wallet.cpp
+++ src/wallet/rpc/wallet.cpp
@@ -454,7 +454,10 @@ static RPCHelpMan loadwallet()
             RPCResult::Type::OBJ, "", "",
             {
                 {RPCResult::Type::STR, "name", "The wallet name if loaded successfully."},
-                {RPCResult::Type::STR, "warning", "Warning message if wallet was not loaded cleanly."},
+                {RPCResult::Type::ARR, "warnings", /*optional=*/true, "Warning messages, if any, related to loading the wallet.",
+                {
+                    {RPCResult::Type::STR, "", ""},
+                }},
             }
         },
         RPCExamples{
@@ -479,7 +482,13 @@ static RPCHelpMan loadwallet()
 
     UniValue obj(UniValue::VOBJ);
     obj.pushKV("name", wallet->GetName());
-    obj.pushKV("warning", Join(warnings, Untranslated("\n")).original);
+    UniValue warnings_array(UniValue::VARR);
+    for (const auto& warning : warnings) {
+        warnings_array.push_back(warning.original);
+    }
+    if (!warnings_array.empty()) {
+        obj.pushKV("warnings", warnings_array);
+    }
 
     return obj;
 },
@@ -572,7 +581,10 @@ static RPCHelpMan createwallet()
             RPCResult::Type::OBJ, "", "",
             {
                 {RPCResult::Type::STR, "name", "The wallet name if created successfully. If the wallet was created using a full path, the wallet_name will be the full path."},
-                {RPCResult::Type::STR, "warning", "Warning message if wallet was not loaded cleanly."},
+                {RPCResult::Type::ARR, "warnings", /*optional=*/true, "Warning messages, if any, related to creating and loading the wallet.",
+                {
+                    {RPCResult::Type::STR, "", ""},
+                }},
             }
         },
         RPCExamples{
@@ -639,7 +651,13 @@ static RPCHelpMan createwallet()
 
     UniValue obj(UniValue::VOBJ);
     obj.pushKV("name", wallet->GetName());
-    obj.pushKV("warning", Join(warnings, Untranslated("\n")).original);
+    UniValue warnings_array(UniValue::VARR);
+    for (const auto& warning : warnings) {
+        warnings_array.push_back(warning.original);
+    }
+    if (!warnings_array.empty()) {
+        obj.pushKV("warnings", warnings_array);
+    }
 
     return obj;
 },
@@ -656,7 +674,10 @@ static RPCHelpMan unloadwallet()
             {"load_on_startup", RPCArg::Type::BOOL, RPCArg::Optional::OMITTED_NAMED_ARG, "Save wallet name to persistent settings and load on startup. True to add wallet to startup list, false to remove, null to leave unchanged."},
         },
         RPCResult{RPCResult::Type::OBJ, "", "", {
-            {RPCResult::Type::STR, "warning", "Warning message if wallet was not unloaded cleanly."},
+            {RPCResult::Type::ARR, "warnings", /*optional=*/true, "Warning messages, if any, related to unloading the wallet.",
+            {
+                {RPCResult::Type::STR, "", ""},
+            }},
         }},
         RPCExamples{
             HelpExampleCli("unloadwallet", "wallet_name")
@@ -698,7 +719,13 @@ static RPCHelpMan unloadwallet()
     UnloadWallet(std::move(wallet));
 
     UniValue result(UniValue::VOBJ);
-    result.pushKV("warning", Join(warnings, Untranslated("\n")).original);
+    UniValue warnings_array(UniValue::VARR);
+    for (const auto& warning : warnings) {
+        warnings_array.push_back(warning.original);
+    }
+    if (!warnings_array.empty()) {
+        result.pushKV("warnings", warnings_array);
+    }
     return result;
 },
     };
