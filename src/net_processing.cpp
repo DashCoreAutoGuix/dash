2037fa3c61dc9e2d4fa518e0e3f2002d6ef258a5
fanquake <fanquake@gmail.com>
2023-06-26 10:25:42 +0100

    Merge bitcoin/bitcoin#27947: MaybePunishNodeForTx: Remove unused message arg and logging
    9fe5f6d5d1ec31ebfacfe23368f22c2a0b58832d MaybePunishNodeForTx: Remove unused message arg and logging (Greg Sanders)
    
    Pull request description:
    
    ACKs for top commit:
      MarcoFalke:
        lgtm ACK 9fe5f6d5d1ec31ebfacfe23368f22c2a0b58832d 🕚
      dergoegge:
        utACK 9fe5f6d5d1ec31ebfacfe23368f22c2a0b58832d
    
    Tree-SHA512: c40e39b0f6bd738675dadc707a2f5823aad8ddf532afa956579899f2b76edc6fb0bdc1ed78b4ed2f659ffe02c85d1d9fc16667f131e92562c2c0541bd8eee9bd

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -700,7 +700,7 @@ private:
      *
      * Changes here may need to be reflected in TxRelayMayResultInDisconnect().
      */
-    bool MaybePunishNodeForTx(NodeId nodeid, const TxValidationState& state, const std::string& message = "")
+    bool MaybePunishNodeForTx(NodeId nodeid, const TxValidationState& state)
         EXCLUSIVE_LOCKS_REQUIRED(!m_peer_mutex);
 
     /** Maybe disconnect a peer and discourage future connections from its address.
@@ -1868,7 +1868,7 @@ bool PeerManagerImpl::MaybePunishNodeForBlock(NodeId nodeid, const BlockValidati
     return false;
 }
 
-bool PeerManagerImpl::MaybePunishNodeForTx(NodeId nodeid, const TxValidationState& state, const std::string& message) {
+bool PeerManagerImpl::MaybePunishNodeForTx(NodeId nodeid, const TxValidationState& state) {
     switch (state.GetResult()) {
     case TxValidationResult::TX_RESULT_UNSET:
         break;
@@ -1876,7 +1876,7 @@ bool PeerManagerImpl::MaybePunishNodeForTx(NodeId nodeid, const TxValidationStat
     case TxValidationResult::TX_CONSENSUS:
         {
             LOCK(cs_main);
-            Misbehaving(nodeid, 100, message);
+            Misbehaving(nodeid, 100, "");
             return true;
         }
     // Conflicting (but not necessarily invalid) data or different policy:
@@ -1892,9 +1892,6 @@ bool PeerManagerImpl::MaybePunishNodeForTx(NodeId nodeid, const TxValidationStat
     case TxValidationResult::TX_BAD_SPECIAL:
     case TxValidationResult::TX_CONFLICT_LOCK:
         break;
-    }
-    if (message != "") {
-        LogPrint(BCLog::NET, "peer=%d: %s\n", nodeid, message);
     }
     return false;
 }
