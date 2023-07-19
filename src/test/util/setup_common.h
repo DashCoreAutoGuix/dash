2db545ef858db3ecdba0516bb1af67be378addf1
fanquake <fanquake@gmail.com>
2023-07-19 10:10:45 +0100

    Merge bitcoin/bitcoin#27425: test: move remaining rand code from util/setup_common to util/random
    1cd45d4e08c3dfd1d6423620c79169f1404ac12b test: move random.h include header from setup_common.h to cpp (Jon Atack)
    1b246fdd145a95f5da479159f5e8eaf5a76bdc3a test: move remaining random test util code from setup_common to random (jonatack)
    
    Pull request description:
    
      and drop the `util/random` dependency on `util/setup_common`.  This improves code separation and allows `util/setup_common` to call `util/random` functions without creating a circular dependency, thereby addressing https://github.com/bitcoin/bitcoin/pull/26940#issuecomment-1497266140 by glozow (thanks!)
    
    ACKs for top commit:
      MarcoFalke:
        lgtm ACK 1cd45d4e08c3dfd1d6423620c79169f1404ac12b 🌂
    
    Tree-SHA512: 6ce63d9103ba9b04eebbd8ad02fe9aa79e356296533404034a1ae88e9b7ca0bc9a5c51fd754b71cf4e7b55b18bcd4d5474b2d588edee3851e3b3ce0e4d309a93

--- src/test/util/setup_common.h
+++ src/test/util/setup_common.h
@@ -14,7 +14,9 @@
 #include <node/context.h> // IWYU pragma: export
 #include <pubkey.h>
 #include <random.h>
+#include <stdexcept>
 #include <txmempool.h>
+#include <util/chaintype.h>
 #include <util/check.h>
 #include <util/string.h>
 #include <util/time.h>
@@ -26,6 +28,9 @@
 #include <vector>
 
 class CChainParams;
+class CFeeRate;
+class Chainstate;
+class FastRandomContext;
 namespace Consensus {
 struct Params;
 };
@@ -44,43 +49,6 @@ std::ostream& operator<<(typename std::enable_if<std::is_enum<T>::value, std::os
     return stream << static_cast<typename std::underlying_type<T>::type>(e);
 }
 } // namespace std
-
-/**
- * This global and the helpers that use it are not thread-safe.
- *
- * If thread-safety is needed, the global could be made thread_local (given
- * that thread_local is supported on all architectures we support) or a
- * per-thread instance could be used in the multi-threaded test.
- */
-extern FastRandomContext g_insecure_rand_ctx;
-
-/**
- * Flag to make GetRand in random.h return the same number
- */
-extern bool g_mock_deterministic_tests;
-
-enum class SeedRand {
-    ZEROS, //!< Seed with a compile time constant of zeros
-    SEED,  //!< Call the Seed() helper
-};
-
-/** Seed the given random ctx or use the seed passed in via an environment var */
-void Seed(FastRandomContext& ctx);
-
-static inline void SeedInsecureRand(SeedRand seed = SeedRand::SEED)
-{
-    if (seed == SeedRand::ZEROS) {
-        g_insecure_rand_ctx = FastRandomContext(/*fDeterministic=*/true);
-    } else {
-        Seed(g_insecure_rand_ctx);
-    }
-}
-
-static inline uint32_t InsecureRand32() { return g_insecure_rand_ctx.rand32(); }
-static inline uint256 InsecureRand256() { return g_insecure_rand_ctx.rand256(); }
-static inline uint64_t InsecureRandBits(int bits) { return g_insecure_rand_ctx.randbits(bits); }
-static inline uint64_t InsecureRandRange(uint64_t range) { return g_insecure_rand_ctx.randrange(range); }
-static inline bool InsecureRandBool() { return g_insecure_rand_ctx.randbool(); }
 
 static constexpr CAmount CENT{1000000};
 
