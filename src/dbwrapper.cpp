303d267a41994948ff0a14ea51c473a23144231f
fanquake <fanquake@gmail.com>
2023-08-07 17:06:42 +0200

    Merge bitcoin/bitcoin#26296: ci: Integrate `bitcoin-tidy` clang-tidy plugin
    1c976c691cc4b20f43071aabf36c7afed1571057 tidy: Integrate bicoin-tidy clang-tidy plugin (fanquake)
    7de23cceb8ac13fcc709453ef0fa14fb93c460b0 refactor: fix unterminated LogPrintf()s (fanquake)
    0a1029aa2946787bfe91a84de882c2dd0de076e9 lint: remove  /* Continued */ markers from codebase (fanquake)
    910007995d8603ffc466878856227153a638caff lint: remove lint-logs.py (fanquake)
    d86a83d6b8587b0971e66c6910af23dd8c042969 lint: drop DIR_IWYU global (fanquake)
    
    Pull request description:
    
      Demo of integrating the [bitcoin-tidy](https://github.com/theuni/bitcoin-tidy-plugin), [clang-tidy plugin](https://clang.llvm.org/extra/clang-tidy/) written by theuni into our tidy CI job.
    
      The plugin currently has a single check, `bitcoin-unterminated-logprintf`. This would replace our current Python driven, `git-grep`-based, `.cpp` file only, lint-logs linter.
    
    ACKs for top commit:
      TheCharlatan:
        ACK 1c976c691cc4b20f43071aabf36c7afed1571057
      theuni:
        ACK 1c976c691cc4b20f43071aabf36c7afed1571057
      MarcoFalke:
        re-ACK 1c976c691cc4b20f43071aabf36c7afed1571057  👠
    
    Tree-SHA512: 725b45c70e431d48e6f276671e05c694e10b6047cae1a31906ac3ee9093bc8105fb226b36a5bac6709557526ca6007222112d66aecec05a574434edc4897e4b8

--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -64,7 +64,7 @@ public:
 
                 assert(p <= limit);
                 base[std::min(bufsize - 1, (int)(p - base))] = '\0';
-                LogPrintLevel(BCLog::LEVELDB, BCLog::Level::Debug, "%s", base);  /* Continued */
+                LogPrintLevel(BCLog::LEVELDB, BCLog::Level::Debug, "%s", base); // NOLINT(bitcoin-unterminated-logprintf)
                 if (base != buffer) {
                     delete[] base;
                 }
