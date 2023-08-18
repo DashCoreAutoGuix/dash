ce1bccdc783a3159bd3004d8e6182b0a8eec9e9a
fanquake <fanquake@gmail.com>
2023-08-18 14:39:36 +0100

    Merge bitcoin/bitcoin#28295: ci: Add missing amd64 to win64-cross task
    fa56d17a4b868f42fa45bea0d1f0c78de75e7838 ci: Add missing amd64 to win64-cross task (MarcoFalke)
    
    Pull request description:
    
      Currently the task will fail if run on non-`x86_64`.
    
      Fix this by adding the missing `amd64`, similar to
    
      https://github.com/bitcoin/bitcoin/blob/7bf078f2b7d4a0339d053144b4fb35fe020dac25/ci/test/00_setup_env_i686_multiprocess.sh#L11
    
    ACKs for top commit:
      hebasto:
        ACK fa56d17a4b868f42fa45bea0d1f0c78de75e7838
    
    Tree-SHA512: faab1c5b945283b7e8d080bbcc8e9379c480cf6973506149ace5990cb4d04673f83f4bc36d08d5b4e9cb17a86fdbe23ac97ef4eab0e842616b367b8138229c58

--- ci/test/00_setup_env_win64.sh
+++ ci/test/00_setup_env_win64.sh
@@ -7,6 +7,7 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_win64
+export CI_IMAGE_NAME_TAG="docker.io/amd64/ubuntu:22.04"  # Check that Jammy can cross-compile to win64
 export HOST=x86_64-w64-mingw32
 export DPKG_ADD_ARCH="i386"
 export PACKAGES="python3 nsis g++-mingw-w64-x86-64-posix wine-binfmt wine64 wine32 file"
