57472f0167380cee4205ec3a5bc29f00c20b694d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-09-22 15:23:24 +0100

    Merge bitcoin-core/gui#739: Disable and uncheck blank when private keys are disabled
    9ea31eba04ff8dcb9d7d993ce28bb10731a35177 gui: Disable and uncheck blank when private keys are disabled (Andrew Chow)
    
    Pull request description:
    
      Unify the GUI's create wallet with the RPC createwallet so that the blank flag is not set when private keys are disabled.
    
    ACKs for top commit:
      S3RK:
        Code review ACK 9ea31eba04ff8dcb9d7d993ce28bb10731a35177
      jarolrod:
        ACK 9ea31eba04ff8dcb9d7d993ce28bb10731a35177
      pablomartin4btc:
        tACK 9ea31eba04ff8dcb9d7d993ce28bb10731a35177
    
    Tree-SHA512: 0c90dbd77e66f088c6a57711a4b91e254814c4ee301ab703807f281cacd4b08712d2dfeac7661f28bc0e93acc55d486a17b8b4a53ffa57093d992e7a3c51f4e8

--- src/qt/createwalletdialog.cpp
+++ src/qt/createwalletdialog.cpp
@@ -41,9 +41,10 @@ CreateWalletDialog::CreateWalletDialog(QWidget* parent) :
         // set to true, enable it when isDisablePrivateKeysChecked is false.
         ui->encrypt_wallet_checkbox->setEnabled(!checked);
 
-        // Wallets without private keys start out blank
+        // Wallets without private keys cannot set blank
+        ui->blank_wallet_checkbox->setEnabled(!checked);
         if (checked) {
-            ui->blank_wallet_checkbox->setChecked(true);
+            ui->blank_wallet_checkbox->setChecked(false);
         }
 
         // When the encrypt_wallet_checkbox is disabled, uncheck it.
@@ -53,8 +54,11 @@ CreateWalletDialog::CreateWalletDialog(QWidget* parent) :
     });
 
     connect(ui->blank_wallet_checkbox, &QCheckBox::toggled, [this](bool checked) {
-        if (!checked) {
-          ui->disable_privkeys_checkbox->setChecked(false);
+        // Disable the disable_privkeys_checkbox when blank_wallet_checkbox is checked
+        // as blank-ness only pertains to wallets with private keys.
+        ui->disable_privkeys_checkbox->setEnabled(!checked);
+        if (checked) {
+            ui->disable_privkeys_checkbox->setChecked(false);
         }
     });
 
