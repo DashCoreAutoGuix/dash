4aeea1c5265cbd1ef5c940e27ef25029b66b1195
fanquake <fanquake@gmail.com>
2023-10-02 11:58:48 +0200

    Merge bitcoin/bitcoin#28500: Prevent default/invalid CKey objects from allocating secure memory
    6ef405ddb195bbf1b28a906d8c8bb877f0c17d7b key: don't allocate secure mem for null (invalid) key (Pieter Wuille)
    d9841a7ac634472c1a9105f81f8e7b55e4bd1a4a Add make_secure_unique helper (Anthony Towns)
    
    Pull request description:
    
      Bitcoin Core has `secure_allocator`, which allocates inside special "secure" (non-swappable) memory pages, which may be limited in availability. Currently, every `CKey` object uses 32 such secure bytes, even when the `CKey` object contains the (invalid) value zero.
    
      Change this to not use memory when the `CKey` is invalid. This is particularly relevant for `BIP324Cipher` which briefly holds a `CKey`, but after receiving the remote's public key and initializing the encryption ciphers, the key is wiped. In case secure memory usage is in high demand, it'd be silly to waste it on P2P encryption keys instead of wallet keys.
    
    ACKs for top commit:
      ajtowns:
        ACK 6ef405ddb195bbf1b28a906d8c8bb877f0c17d7b
      john-moffett:
        ACK 6ef405ddb195bbf1b28a906d8c8bb877f0c17d7b
    
    Tree-SHA512: 987f4376ed825daf034ea4d7c4b4952fe664b25b48f1c09fbcfa6257a40b06c4da7c2caaafa35c346c86bdf298ae21f16c68ea4b1039836990d1a205de2034fd

--- src/support/allocators/secure.h
+++ src/support/allocators/secure.h
@@ -28,7 +28,7 @@ struct secure_allocator {
     T* allocate(std::size_t n)
     {
         T* allocation = static_cast<T*>(LockedPoolManager::Instance().alloc(sizeof(T) * n));
-        if (!allocation) {
+        if (\!allocation) {
             throw std::bad_alloc();
         }
         return allocation;
@@ -36,7 +36,7 @@ struct secure_allocator {
 
     void deallocate(T* p, std::size_t n)
     {
-        if (p != nullptr) {
+        if (p \!= nullptr) {
             memory_cleanse(p, sizeof(T) * n);
         }
         LockedPoolManager::Instance().free(p);
@@ -48,7 +48,7 @@ struct secure_allocator {
         return true;
     }
     template <typename U>
-    friend bool operator!=(const secure_allocator&, const secure_allocator<U>&) noexcept
+    friend bool operator\!=(const secure_allocator&, const secure_allocator<U>&) noexcept
     {
         return false;
     }
@@ -59,5 +59,29 @@ struct secure_allocator {
 typedef std::basic_string<char, std::char_traits<char>, secure_allocator<char> > SecureString;
 
 typedef std::vector<unsigned char, secure_allocator<unsigned char> > SecureVector;
+
+template<typename T>
+struct SecureUniqueDeleter {
+    void operator()(T* t) noexcept {
+        secure_allocator<T>().deallocate(t, 1);
+    }
+};
+
+template<typename T>
+using secure_unique_ptr = std::unique_ptr<T, SecureUniqueDeleter<T>>;
+
+template<typename T, typename... Args>
+secure_unique_ptr<T> make_secure_unique(Args&&... as)
+{
+    T* p = secure_allocator<T>().allocate(1);
+
+    // initialize in place, and return as secure_unique_ptr
+    try {
+        return secure_unique_ptr<T>(new (p) T(std::forward(as)...));
+    } catch (...) {
+        secure_allocator<T>().deallocate(p, 1);
+        throw;
+    }
+}
 
 #endif // BITCOIN_SUPPORT_ALLOCATORS_SECURE_H
