c21e011fc1bd4658d4eaef91c8d8c998985ed8e6
fanquake <fanquake@gmail.com>
2023-10-13 11:07:25 +0200

    Merge bitcoin/bitcoin#28639: refactor: Remove unused nchaintx from SnapshotMetadata constructor, fix test, add test
    fafde92f84fb7c245bc3c1cd946a32c891861e5e test: Check snapshot file with wrong number of coins (MarcoFalke)
    faa90f6e7b6b8c531e1ae142a5c2f568b48502a9 refactor: Remove unused nchaintx from SnapshotMetadata constructor (MarcoFalke)
    
    Pull request description:
    
      See commit messages
    
    ACKs for top commit:
      Sjors:
        utACK fafde92f84fb7c245bc3c1cd946a32c891861e5e
      theStack:
        ACK fafde92f84fb7c245bc3c1cd946a32c891861e5e
    
    Tree-SHA512: 9ed2720b50d1c0938f30543ba143e1a4c6af3a0ff166f8b3eb452e1d99ddee6e3443a4c99f77efe94b8c3eb2feff984bf5259807ee8085e1e0e1e0d1de98227e

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -2722,7 +2722,7 @@ UniValue CreateUTXOSnapshot(
         tip->nHeight, tip->GetBlockHash().ToString(),
         fs::PathToString(path), fs::PathToString(temppath)));
 
-    SnapshotMetadata metadata{tip->GetBlockHash(), stats.coins_count, tip->nChainTx};
+    SnapshotMetadata metadata{tip->GetBlockHash(), stats.coins_count};
 
     afile << metadata;
 
@@ -2747,11 +2747,17 @@ UniValue CreateUTXOSnapshot(
     result.pushKV("coins_written", stats.coins_count);
     result.pushKV("base_hash", tip->GetBlockHash().ToString());
     result.pushKV("base_height", tip->nHeight);
+
     result.pushKV("path", path.utf8string());
     result.pushKV("txoutset_hash", stats.hashSerialized.ToString());
     // Cast required because univalue doesn't have serialization specified for
     // `unsigned int`, nChainTx's type.
     result.pushKV("nchaintx", uint64_t{tip->nChainTx});
+
+    result.pushKV("path", path.u8string());
+    result.pushKV("txoutset_hash", maybe_stats->hashSerialized.ToString());
+    result.pushKV("nchaintx", tip->nChainTx);
+
     return result;
 }
 
