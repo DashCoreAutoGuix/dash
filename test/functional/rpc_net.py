8d8c247dfb503ca45d1872d602efa72b51e54245
fanquake <fanquake@gmail.com>
2023-10-19 09:32:12 +0100

    Merge bitcoin/bitcoin#28671: test: Fix failing time check in rpc_net.py
    fa4c6836c9366c3cc575cb386a397840d5f1aa57 test: Fix failing time check in rpc_net.py (MarcoFalke)
    
    Pull request description:
    
      This check fails on slow runners, such as s390x qemu.
    
      Fix it by using mocktime.
    
      See https://github.com/bitcoin/bitcoin/pull/28523#discussion_r1357980527
    
    ACKs for top commit:
      0xB10C:
        ACK fa4c6836c9366c3cc575cb386a397840d5f1aa57
      pinheadmz:
        ACK fa4c6836c9366c3cc575cb386a397840d5f1aa57
      brunoerg:
        crACK fa4c6836c9366c3cc575cb386a397840d5f1aa57
    
    Tree-SHA512: 83fb534682e11e97537dc89de8c16f206f38af1a892a2d5970c02684c05eaea8fc9adba3159f16b2440ca0b3871d513a0562a6f3a38f19a5574a47be0919e42f

--- test/functional/rpc_net.py
+++ test/functional/rpc_net.py
@@ -15,6 +15,7 @@ from test_framework.messages import (
 )
 
 from itertools import product
+import time
 
 from test_framework.test_framework import DashTestFramework
 from test_framework.util import (
@@ -342,6 +343,8 @@ class NetTest(DashTestFramework):
         assert_equal(node.getnodeaddresses(count=0), [])
 
         self.log.debug("Test that adding a valid address to the tried table succeeds")
+        self.addr_time = int(time.time())
+        node.setmocktime(self.addr_time)
         assert_equal(node.addpeeraddress(address="1.2.3.4", tried=True, port=8333), {"success": True})
         with node.assert_debug_log(expected_msgs=["CheckAddrman: new 0, tried 1, total 1 started"]):
             addrs = node.getnodeaddresses(count=0)  # getnodeaddresses re-runs the addrman checks
