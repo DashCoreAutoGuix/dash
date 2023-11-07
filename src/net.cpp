cad52720b4e2dbf8cbf873fea14e5691b55b69af
Andrew Chow <github@achow101.com>
2023-11-07 14:11:46 -0500

    Merge bitcoin/bitcoin#28649: Do the SOCKS5 handshake reliably
    af0fca530e4d8311bcb24a14c416e5ad7c30ff78 netbase: use reliable send() during SOCKS5 handshake (Vasil Dimov)
    1b19d1117ca5373a15313227b547ef4392022dbd sock: change Sock::SendComplete() to take Span (Vasil Dimov)
    
    Pull request description:
    
      The `Socks5()` function which does the SOCKS5 handshake with the SOCKS5 proxy sends bytes to the socket without retrying partial writes.
    
      `send(2)` may write only part of the provided data and return. In this case the caller is responsible for retrying the operation with the remaining data. Change `Socks5()` to do that. There is already a method `Sock::SendComplete()` which does exactly that, so use it in `Socks5()`.
    
      A minor complication for this PR is that `Sock::SendComplete()` takes `std::string` argument whereas `Socks5()` has `std::vector<uint8_t>`. Thus the necessity for the first commit. It is possible to do also in other ways - convert the data in `Socks5()` to `std::string` or have just one `Sock::SendComplete()` that takes `void*` and change the callers to pass `str.data(), str.size()` or `vec.data(), vec.size()`.
    
      This came up while testing https://github.com/bitcoin/bitcoin/pull/27375.
    
    ACKs for top commit:
      achow101:
        ACK af0fca530e4d8311bcb24a14c416e5ad7c30ff78
      jonatack:
        ACK af0fca530e4d8311bcb24a14c416e5ad7c30ff78
      pinheadmz:
        ACK af0fca530e4d8311bcb24a14c416e5ad7c30ff78
    
    Tree-SHA512: 1d4a53d0628f7607378038ac56dc3b8624ce9322b034c9547a0c3ce052eafb4b18213f258aa3b57bcb4d990a5e0548a37ec70af2bd55f6e8e6399936f1ce047a

--- src/net.cpp
+++ src/net.cpp
@@ -4003,7 +4003,6 @@ bool CConnman::Start(CDeterministicMNManager& dmnman, CMasternodeMetaMan& mn_met
     // Start threads
     //
     assert(m_msgproc);
-    InterruptSocks5(false);
     interruptNet.reset();
     flagInterruptMsgProc = false;
 
@@ -4096,7 +4095,7 @@ void CConnman::Interrupt()
     condMsgProc.notify_all();
 
     interruptNet();
-    InterruptSocks5(true);
+    g_socks5_interrupt();
 
     if (semOutbound) {
         for (int i=0; i<m_max_outbound; i++) {
--- src/netbase.h
+++ src/netbase.h
@@ -13,6 +13,7 @@
 #include <netaddress.h>
 #include <serialize.h>
 #include <util/sock.h>
+#include <util/threadinterrupt.h>
 
 #include <functional>
 #include <memory>
@@ -306,7 +307,15 @@ std::unique_ptr<Sock> ConnectThroughProxy(const Proxy& proxy,
                                           uint16_t port,
                                           bool& proxy_connection_failed);
 
-void InterruptSocks5(bool interrupt);
+/**
+ * Timeout for SOCKS5 negotiation
+ */
+extern std::chrono::milliseconds g_socks5_recv_timeout;
+
+/**
+ * Interrupt SOCKS5 reads or writes.
+ */
+extern CThreadInterrupt g_socks5_interrupt;
 
 /**
  * Connect to a specified destination service through an already connected
