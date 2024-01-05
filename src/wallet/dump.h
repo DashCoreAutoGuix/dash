30b2c961b5d6e240ff02336c37874e7a176a8702
fanquake <fanquake@gmail.com>
2024-01-05 17:35:18 +0000

    Merge bitcoin/bitcoin#29117: wallettool: Always be able to dump a wallet's database
    d83bea42d1f0ffb0899a6de3556c489543468995 wallettool: Don't create CWallet when dumping DB (Andrew Chow)
    40c80e36b1a204ed133acc403016a6cb1a92051e wallettool: Don't unilaterally reset wallet_instance if loading error (Ava Chow)
    
    Pull request description:
    
      https://github.com/bitcoin/bitcoin/issues/29109#issuecomment-1863449058 reports that a wallet with noncritical errors cannot be dumped with `bitcoin-wallet dump`. This was caused by an erroneous reset of the wallet pointer when the loading the wallet returns something other than `LOAD_OK`. Not all errors are errors that require aborting, so unilaterally resetting the pointer at that time is incorrect. The first commit resolves this issue.
    
      Furthermore, if a wallet has loading errors, that should not prevent the wallet tool from dumping the wallet. The wallet application logic should not get in the way of performing such a low level database operation, especially when it's primary usage is for debugging potentially corrupted wallets. The 2nd commit is taken from #28710 and changes the `dump` to stop at making a `WalletDatabase` rather than making a `CWallet` only to retrieve the underlying `WalletDatabase`.
    
    ACKs for top commit:
      furszy:
        Code review ACK d83bea42d1
      BrandonOdiwuor:
        Code Review ACK d83bea42d1f0ffb0899a6de3556c489543468995
    
    Tree-SHA512: 425d712dfff1002bd81272aca0bae1016f9126a3c89506f8cb7cf0a0ec9f33d0c03b8d03896394f3a45c2998e59047e19218dfd08dc8a5f40e8625134e886b0f

--- src/wallet/dump.h
+++ src/wallet/dump.h
@@ -14,10 +14,11 @@ struct bilingual_str;
 class ArgsManager;
 
 namespace wallet {
-class CWallet;
+class WalletDatabase;
 
-bool DumpWallet(const ArgsManager& args, CWallet& wallet, bilingual_str& error);
+bool DumpWallet(const ArgsManager& args, WalletDatabase& db, bilingual_str& error);
 bool CreateFromDump(const ArgsManager& args, const std::string& name, const fs::path& wallet_path, bilingual_str& error, std::vector<bilingual_str>& warnings);
 } // namespace wallet
 
 #endif // BITCOIN_WALLET_DUMP_H
+EOF < /dev/null
