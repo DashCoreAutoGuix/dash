af71e496d25f151358ad64ed1d3b6eba3d0b0797
Ava Chow <github@achow101.com>
2024-01-23 14:20:39 -0500

    Merge bitcoin/bitcoin#29272: wallet: fix coin selection tracing to return -1 when no change pos
    d55fdb1a495190e213b1b5127f5d91e4a409765e Move TRACEx parameters to seperate lines (Richard Myers)
    2d58629ee63eebc760e2a9226afcd0c46d3ec2bd wallet: fix coin selection tracing to return -1 when no change pos (Richard Myers)
    
    Pull request description:
    
      This is a bugfix for from when [optional was introduced](https://github.com/bitcoin/bitcoin/pull/25273/commits/758501b71391136c33b525b1a0109b990d4f463e)  for `change_pos` in the wallet. When optional `change_pos` is unset, we should return -1 and not 0.
    
      I added two new checks to the `test/functional/interface_usdt_coinselection.py` which adds coverage for the situations when `normal_create_tx_internal` and `aps_create_tx_internal` events occur with no change.
    
      You can reproduce this bug using the coin-selection-simulation scripts as described in [issue #16](https://github.com/achow101/coin-selection-simulation/issues/16). You can also run the `interface_usdt_coinselection.py` test  without the changes to `wallet/spend.cpp`.
    
    ACKs for top commit:
      0xB10C:
        ACK d55fdb1a495190e213b1b5127f5d91e4a409765e
      achow101:
        ACK d55fdb1a495190e213b1b5127f5d91e4a409765e
      murchandamus:
        ACK d55fdb1a495190e213b1b5127f5d91e4a409765e
    
    Tree-SHA512: 6efac3b756bdf51debbcb759dc3c4b7a4304626bc047b70025cec02f3a04937ace7712e9558ac71e560fd136005a98c518ac5bb4b90c3282d776beccd0de9749

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -1051,7 +1051,7 @@ std::optional<CreatedTransactionResult> CreateTransaction(
 
     std::optional<CreatedTransactionResult> txr_ungrouped = CreateTransactionInternal(wallet, vecSend, change_pos, error, coin_control, fee_calc_out, sign, nExtraPayloadSize);
     TRACE4(coin_selection, normal_create_tx_internal, wallet.GetName().c_str(), txr_ungrouped.has_value(),
-           txr_ungrouped.has_value() ? txr_ungrouped->fee : 0, txr_ungrouped.has_value() ? txr_ungrouped->change_pos : 0);
+           txr_ungrouped.has_value() ? txr_ungrouped->fee : 0, txr_ungrouped.has_value() ? txr_ungrouped->change_pos : -1);
     if (!txr_ungrouped) return std::nullopt;
     // try with avoidpartialspends unless it's enabled already
     if (txr_ungrouped->fee > 0 /* 0 means non-functional fee rate estimation */ && wallet.m_max_aps_fee > -1 && !coin_control.m_avoid_partial_spends) {
