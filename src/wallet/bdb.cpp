b16187a5cf1c2b9c5394654545673a2134d1b584
furszy <matiasfurszyfer@protonmail.com>
2024-02-06 16:28:23 -0300

    wallet: bdb batch 'ErasePrefix', do not create txn internally
    Transactions are intended to be started on upper layers rather than
    internally by the bdb batch object. This enables us to consolidate
    different write operations within a procedure in the same db txn,
    improving consistency due to the atomic property of the transaction,
    as well as its performance due to the reduction of disk write
    operations.
    
    Important Note:
    This approach also ensures that the BerkeleyBatch::ErasePrefix
    function behaves exactly as the SQLiteBatch::ErasePrefix function,
    which does not create a db txn internally.
    
    Furthermore, since the `BerkeleyBatch::ErasePrefix' implementation
    erases records one by one (by traversing the db), this change
    ensures that the function is always called within an active txn
    context. Without this measure, there's a potential risk to consistency;
    certain records may be removed while others could persist due to an
    internal failure during the procedure.

--- src/wallet/bdb.cpp
+++ src/wallet/bdb.cpp
@@ -816,6 +816,30 @@ bool BerkeleyBatch::HasKey(CDataStream&& key)
     return ret == 0;
 }
 
+bool BerkeleyBatch::ErasePrefix(Span<const std::byte> prefix)
+{
+    // Because this function erases records one by one, ensure that it is executed within a txn context.
+    // Otherwise, consistency is at risk; it's possible that certain records are removed while others
+    // remain due to an internal failure during the procedure.
+    // Additionally, the Dbc::del() cursor delete call below would fail without an active transaction.
+    if (!Assume(activeTxn)) return false;
+
+    auto cursor{std::make_unique<BerkeleyCursor>(m_database, *this)};
+    // const_cast is safe below even though prefix_key is an in/out parameter,
+    // because we are not using the DB_DBT_USERMEM flag, so BDB will allocate
+    // and return a different output data pointer
+    Dbt prefix_key{const_cast<std::byte*>(prefix.data()), static_cast<uint32_t>(prefix.size())}, prefix_value{};
+    int ret{cursor->dbc()->get(&prefix_key, &prefix_value, DB_SET_RANGE)};
+    for (int flag{DB_CURRENT}; ret == 0; flag = DB_NEXT) {
+        SafeDbt key, value;
+        ret = cursor->dbc()->get(key, value, flag);
+        if (ret != 0 || key.get_size() < prefix.size() || memcmp(key.get_data(), prefix.data(), prefix.size()) != 0) break;
+        ret = cursor->dbc()->del(0);
+    }
+    cursor.reset();
+    return ret == 0 || ret == DB_NOTFOUND;
+}
+
 void BerkeleyDatabase::AddRef()
 {
     LOCK(cs_db);
