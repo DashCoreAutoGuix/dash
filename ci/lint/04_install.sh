81bd4913eabfed95a93a769e2f01bc6b3c0dccbe
fanquake <fanquake@gmail.com>
2024-02-26 10:27:08 +0000

    Merge bitcoin/bitcoin#29408: lint: Check for missing bitcoin-config.h includes
    fa58ae74ea67485495dbc2d003adfbca68d6869b refactor: Add missing include for USE_BDB, USE_SQLITE to bench/wallet_ismine.cpp (MarcoFalke)
    fa31908ea848488ff842f1b9fce6235bb8855ec7 lint: Check for missing or redundant bitcoin-config.h includes (MarcoFalke)
    fa63b0e351dee4782ee19ad46603957ef8d020eb lint: Make lint error easier to spot in output (MarcoFalke)
    fa770fd368e32950dd727e111a5d66e1dbb93716 doc: Add missing RUST_BACKTRACE=1 (MarcoFalke)
    fa100512677587b4e84a8be2235cf6d49b6a0134 lint: Add get_subtrees() helper (MarcoFalke)
    
    Pull request description:
    
      Missing `bitcoin-config.h` includes are problematic, because the build could silently pass, but produce an unintended result. For example, a slower fallback algorithm could be picked, even though `bitcoin-config.h` indicates that a faster feature is available and should be used.
    
      As the build succeeds silently, this problem is not possible to detect with iwyu.
    
      Thus, fix this by using a linter based on grepping the source code.
    
    ACKs for top commit:
      theuni:
        Weak ACK fa58ae74ea67485495dbc2d003adfbca68d6869b.
      TheCharlatan:
        ACK fa58ae74ea67485495dbc2d003adfbca68d6869b
      hebasto:
        ACK fa58ae74ea67485495dbc2d003adfbca68d6869b, tested on Ubuntu 23.10 -- it catches bugs properly. I didn't review rust code changes.
    
    Tree-SHA512: cf4346f81ea5b8c215da6004cb2403d1aaf569589613c305d8ba00329b82b3841da94fe1a69815ce15f2edecbef9b031758ec9b6433564976190e3cf91ec8181

--- ci/lint/04_install.sh
+++ ci/lint/04_install.sh
@@ -8,10 +8,11 @@ export LC_ALL=C
 
 ${CI_RETRY_EXE} apt-get update
 # Lint dependencies:
+# - automake pkg-config libtool (for lint_includes_build_config)
 # - curl/xz-utils (to install shellcheck)
 # - git (used in many lint scripts)
 # - gpg (used by verify-commits)
-${CI_RETRY_EXE} apt-get install -y curl xz-utils git gpg
+${CI_RETRY_EXE} apt-get install -y automake pkg-config libtool curl xz-utils git gpg
 
 if [ -z "${SKIP_PYTHON_INSTALL}" ]; then
     PYTHON_PATH=/tmp/python
