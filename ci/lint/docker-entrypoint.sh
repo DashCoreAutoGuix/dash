57b7031bcff35169cc074d200035bd7c0c77e74a
fanquake <fanquake@gmail.com>
2024-03-25 14:40:58 +0000

    Merge bitcoin/bitcoin#29660: lint: Fix COMMIT_RANGE issues
    fa1146d01b148dd60fcada36a3b37ed37532ce2b lint: Fix COMMIT_RANGE issues (MarcoFalke)
    
    Pull request description:
    
      `COMMIT_RANGE` has problems on forks or local branches:
    
      * When `LOCAL_BRANCH` is set, it assumes the presence of a `master` branch, and that the `master` branch is up-to-date. Both of which may be false. (See also discussion in https://github.com/bitcoin/bitcoin/pull/29274#discussion_r1504226422)
      * When `COMMIT_RANGE` isn't set in `lint-git-commit-check.py`, and `--prev-commits` isn't set either, it has the same (broken) assumptions.
    
      Fix all issues by simply assuming a merge commit exists. This allows to drop `LOCAL_BRANCH`. It also allows to drop `SKIP_EMPTY_NOT_A_PR`, because scripts will already skip an empty range. Finally, it allows to drop `--prev-commits n`, because one can simply say `COMMIT_RANGE='HEAD~n..HEAD'` to achieve the same.
    
    ACKs for top commit:
      Sjors:
        tACK fa1146d01b148dd60fcada36a3b37ed37532ce2b
    
    Tree-SHA512: f1477a38267fd4fdb8d396211a5d6bed5f418798c7edaba43487957aaf726cd45244ccf15187b3dd896d398fa1df3fe0a37323e49cf44d60a2018786ed41e5ba

--- ci/lint/docker-entrypoint.sh
+++ ci/lint/docker-entrypoint.sh
@@ -6,7 +6,7 @@ export LC_ALL=C
 git config --global --add safe.directory /bitcoin
 
 if [ -z "$1" ]; then
-  LOCAL_BRANCH=1 bash -ic "./ci/lint/06_script.sh"
+  bash -ic "./ci/lint/06_script.sh"
 else
   exec "$@"
 fi
