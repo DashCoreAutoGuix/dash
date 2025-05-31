4d85fd18775f5b5702bb385b1165176f34df8b12
PastaBot <156604295+DashCoreAutoGuix@users.noreply.github.com>
2025-05-31 15:25:46 -0500

    Update contrib/guix/libexec/build.sh

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -355,11 +355,8 @@ mkdir -p "$DISTSRC"
                 ;;
         esac
 
-        # copy over the example dash.conf file. if contrib/devtools/gen-dash-conf.sh
-        # has not been run before building, this file will be a stub
-        cp "${DISTSRC}/share/examples/dash.conf" "${DISTNAME}/" 2>/dev/null || true
-
         cp -r "${DISTSRC}/share/rpcauth" "${DISTNAME}/share/"
+        
         # Finally, deterministically produce {non-,}debug binary tarballs ready
         # for release
         case "$HOST" in
