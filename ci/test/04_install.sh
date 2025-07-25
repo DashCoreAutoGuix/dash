bitcoin/bitcoin

Merge bitcoin/bitcoin#27423: ci: fix git dubious permissions error
ed4a8339b8fe796b4668e206d7fb9c2b120f8eb2 ci: fix git dubious permissions error (josibake)

Pull request description:

  fixes https://github.com/bitcoin/bitcoin/pull/27376#issuecomment-1496449588

  this appears to be caused by a more recent version of git being sensitive to mismatched permissions on directories. we didn't notice this before because we were using two separate user accounts to fix up dir permissions in the container , but the second account was removed in #27376

  there might be a more elegant way to do this, but this does the trick and seems to be the way others are fixing this issue around the internets.

ACKs for top commit:
  RandyMcMillan:
    ACK ed4a833
  hebasto:
    re-ACK ed4a8339b8fe796b4668e206d7fb9c2b120f8eb2

Tree-SHA512: dad467deca101a24f3ed34b3e26a1db5099a5bd5c3e9c9a22771c59848f7d7e7843c7386348e6fdf86d5a556e4706e5e20005d7a6637193e1c8aef7a5ff7fb19

ci/test/04_install.sh
@@ -57,6 +57,9 @@ if [ -z "$DANGER_RUN_CI_ON_HOST" ]; then
   export CI_EXEC_CMD_PREFIX="docker exec ${CI_CONTAINER_ID}"
   $CI_EXEC_CMD_PREFIX rsync --archive --stats --human-readable /ci_base_install/ "${BASE_ROOT_DIR}"
   $CI_EXEC_CMD_PREFIX rsync --archive --stats --human-readable /ro_base/ "$BASE_ROOT_DIR"
+  # Fixes permission issues when there is a container UID/GID mismatch with the owner
+  # of the mounted bitcoin src dir.
+  $CI_EXEC_CMD_PREFIX git config --global --add safe.directory "*"
 else
   echo "Running on host system without docker wrapper"
   "${BASE_ROOT_DIR}/ci/test/01_base_install.sh"

DashCoreAutoGuix/dash

ci/test/04_install.sh
@@ -61,6 +61,9 @@ if [ -z "$DANGER_RUN_CI_ON_HOST" ]; then
   docker exec "$DOCKER_ID" chown -R "$LOCAL_USER":"$LOCAL_USER" "${BASE_ROOT_DIR}"
   export DOCKER_CI_CMD_PREFIX_ROOT="docker exec -u 0 $DOCKER_ID"
   export DOCKER_CI_CMD_PREFIX="docker exec -u $LOCAL_UID $DOCKER_ID"
+  # Fixes permission issues when there is a container UID/GID mismatch with the owner
+  # of the mounted dash src dir.
+  $DOCKER_CI_CMD_PREFIX git config --global --add safe.directory "*"
 else
   echo "Running on host system without docker wrapper"
 fi