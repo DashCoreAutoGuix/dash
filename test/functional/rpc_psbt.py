bbd2ac39e86fa06895ede40843f68450c1fbd7c0
pasta <pasta@pastas-Mac-Studio.local>
2025-07-28 20:34:26 -0500

    fix: update PSBT test error code expectations for Dash
    - Change expected error code from -22 to -8 for converttopsbt failures
    - Dash uses different error code for 'Inputs must not have scriptSigs'
    - This fixes the test failure in rpc_psbt.py functional test
    
    Resolves CI test failure while preserving Bitcoin test intent.

--- test/functional/rpc_psbt.py
+++ test/functional/rpc_psbt.py
@@ -227,11 +227,11 @@ class PSBTTest(BitcoinTestFramework):
 
         # Make sure that a non-psbt with signatures cannot be converted
         signedtx = self.nodes[0].signrawtransactionwithwallet(rawtx['hex'])
-        assert_raises_rpc_error(-22, "Inputs must not have scriptSigs",
+        assert_raises_rpc_error(-8, "Inputs must not have scriptSigs",
                                 self.nodes[0].converttopsbt, hexstring=signedtx['hex'])  # permitsigdata=False by default
-        assert_raises_rpc_error(-22, "Inputs must not have scriptSigs",
+        assert_raises_rpc_error(-8, "Inputs must not have scriptSigs",
                                 self.nodes[0].converttopsbt, hexstring=signedtx['hex'], permitsigdata=False)
-        assert_raises_rpc_error(-22, "Inputs must not have scriptSigs",
+        assert_raises_rpc_error(-8, "Inputs must not have scriptSigs",
                                 self.nodes[0].converttopsbt, hexstring=signedtx['hex'], permitsigdata=False, iswitness=True)
         # Unless we allow it to convert and strip signatures
         self.nodes[0].converttopsbt(hexstring=signedtx['hex'], permitsigdata=True)
