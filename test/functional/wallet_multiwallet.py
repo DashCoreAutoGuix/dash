1240f6fc908d6f23021315d0b172cffab8b52d23
pasta <pasta@pastas-Mac-Studio.local>
2025-07-28 20:13:03 -0500

    fix: remove extra changes from bitcoin#26192
    - Remove scope creep: use literal 'regtest' and 'wallet.dat' as in Bitcoin original
    - Fix reviewer feedback: removed self.chain and self.wallet_data_filename usage
    
    Resolves validation issues while preserving Bitcoin intent.

--- test/functional/wallet_multiwallet.py
+++ test/functional/wallet_multiwallet.py
@@ -303,8 +303,8 @@ class MultiWalletTest(BitcoinTestFramework):
         if not self.options.descriptors:
             # This tests the default wallet that BDB makes, so SQLite wallet doesn't need to test this
             # Fail to load duplicate wallets by different ways (directory and filepath)
-            path = os.path.join(self.options.tmpdir, "node0", self.chain, "wallets", self.wallet_data_filename)
-            assert_raises_rpc_error(-35, "Wallet file verification failed. Refusing to load database. Data file '{}' is already loaded.".format(path), self.nodes[0].loadwallet, self.wallet_data_filename)
+            path = os.path.join(self.options.tmpdir, "node0", "regtest", "wallets", "wallet.dat")
+            assert_raises_rpc_error(-35, "Wallet file verification failed. Refusing to load database. Data file '{}' is already loaded.".format(path), self.nodes[0].loadwallet, "wallet.dat")
 
             # Only BDB doesn't open duplicate wallet files. SQLite does not have this limitation. While this may be desired in the future, it is not necessary
             # Fail to load if one wallet is a copy of another
